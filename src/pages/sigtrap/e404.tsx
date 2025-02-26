d22090e52ab2df2e44fa88532d7de7b83c157c3e
埃拉 <ElaBosak233@qq.com>
2025-02-26 14:09:29 +0800

    wip: update

---
+++ src/pages/sigtrap/e404.tsx
@@ -0,0 +1,28 @@
+import { Separator } from "@/components/ui/separator";
+import { cn } from "@/utils";
+import { CircleOff } from "lucide-react";
+
+export default function E404() {
+    return (
+        <div
+            className={cn([
+                "flex-1",
+                "flex",
+                "flex-col",
+                "items-center",
+                "justify-center",
+                "gap-7",
+            ])}
+        >
+            <CircleOff className={cn(["size-32"])} />
+            <div className={cn(["flex", "gap-2", "text-xl"])}>
+                <span>404</span>
+                <Separator orientation={"vertical"} />
+                <span>Not Found</span>
+            </div>
+            <span className={cn(["text-secondary"])}>
+                哎呀，好像走错地方了呢
+            </span>
+        </div>
+    );
+}
