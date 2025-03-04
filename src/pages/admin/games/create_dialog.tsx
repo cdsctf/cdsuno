a9cfb8f411a0f00addc8127181373600b91e99a7
Albert Huang <149759599+Albertknight2023@users.noreply.github.com>
2025-03-04 22:05:50 +0800

    wip: admin games page (#2)

---
+++ src/pages/admin/games/create_dialog.tsx
@@ -0,0 +1,112 @@
+import { createGame } from "@/api/games";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useSharedStore } from "@/storages/shared";
+import { cn } from "@/utils";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CheckIcon, PlusCircle, TypeIcon } from "lucide-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
+
+interface CreateDialogProps {
+    onClose: () => void;
+}
+
+function CreateDialog(props: CreateDialogProps) {
+    const { onClose } = props;
+    const sharedStore = useSharedStore();
+    const [loading, setLoading] = useState<boolean>(false);
+    const formSchema = z.object({
+        title: z.string({
+            message: "请输入标题",
+        }),
+        
+    });
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+        
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        setLoading(true);
+        createGame({
+            ...values,
+            description: "",
+            is_need_write_up:false,
+            is_public: false,
+            
+        })
+            .then((res) => {
+                if (res.code === 200) {
+                    toast.success(`比赛 ${res?.data?.title} 创建成功`);
+                    onClose();
+                }
+            })
+            .finally(() => {
+                sharedStore.setRefresh();
+                setLoading(false);
+            });
+    }
+    return (
+        <Card
+            className={cn([
+                "w-128",
+                "min-h-64",
+                "p-5",
+                "flex",
+                "flex-col",
+                "gap-5",
+            ])}
+            
+        >
+            <h3 className={cn(["flex", "gap-3", "items-center", "text-md"])}>
+                <PlusCircle className={cn(["size-4"])} />
+                创建题目
+            </h3>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    autoComplete={"off"}
+                    className={cn(["flex", "flex-col", "flex-1", "gap-5"])}
+                >
+                <FormField
+                        control={form.control}
+                        name={"title"}
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>标题</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        {...field}
+                                        icon={TypeIcon}
+                                        size={"sm"}
+                                        placeholder={"比赛名称"}
+                                        value={field.value || ""}
+                                        onChange={field.onChange}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </form>
+                <Button
+                        variant={"solid"}
+                        icon={CheckIcon}
+                        level={"success"}
+                        loading={loading}
+                    >
+                        确定
+                    </Button>
+            </Form>
+        </Card>
+    )
+}
+export { CreateDialog };
--- src/pages/admin/games/index.tsx
+++ src/pages/admin/games/index.tsx
@@ -1,3 +1,253 @@
+import { Game } from "@/models/game";
+import {
+    ColumnFiltersState,
+    flexRender,
+    getCoreRowModel,
+    getFilteredRowModel,
+    SortingState,
+    useReactTable,
+    VisibilityState,
+} from "@tanstack/react-table";
+import { useEffect, useState } from "react";
+import { columns } from "./columns";
+import { getGames } from "@/api/games";
+import { useDebounce } from "@/hooks/use-debounce";
+import {
+    Flag,
+    HashIcon,
+    ListOrderedIcon,
+    PlusCircle,
+    TypeIcon,
+} from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/utils";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table";
+import { Select } from "@/components/ui/select";
+import { Pagination } from "@/components/ui/pagination";
+import { CreateDialog } from "./create_dialog";
 export default function Index() {
-    return <div>games</div>;
+    const [createDialogOpen, setCreateDialogOpen] = useState<boolean>(false);
+
+    const [page, setPage] = useState<number>(1);
+    const [size, setSize] = useState<number>(20);
+    const [total, setTotal] = useState<number>(0);
+    const [sorting, setSorting] = useState<SortingState>([]);
+
+    const [columnVisibility, setColumnVisibility] = useState<VisibilityState>(
+        {}
+    );
+    const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([
+        {
+            id: "id",
+            value: "all",
+        },
+    ]);
+    const debouncedColumnFilters = useDebounce(columnFilters, 100);
+    const [games, setGames] = useState<Array<Game>>([]);
+    const table = useReactTable<Game>({
+        data: games,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        manualPagination: true,
+        rowCount: total,
+        manualFiltering: true,
+        getFilteredRowModel: getFilteredRowModel(),
+        onColumnFiltersChange: setColumnFilters,
+        onColumnVisibilityChange: setColumnVisibility,
+        manualSorting: true,
+        onSortingChange: setSorting,
+        state: {
+            sorting,
+            columnVisibility,
+        },
+    });
+
+    useEffect(() => {
+        getGames({
+            id: debouncedColumnFilters.find((c) => c.id === "id")
+                ?.value as number,
+            title: debouncedColumnFilters.find((c) => c.id === "title")
+                ?.value as string,
+            sorts: sorting
+                .map((value) => (value.desc ? `-${value.id}` : `${value.id}`))
+                .join(","),
+            page,
+            size,
+        }).then((res) => {
+            setTotal(res?.total || 0);
+            setGames(res?.data || []);
+        });
+    }, [page, size, sorting, debouncedColumnFilters]);
+    return (
+        <div className="container mx-auto py-10">
+            <div
+                className={cn([
+                    "flex",
+                    "justify-between",
+                    "items-center",
+                    "mb-6",
+                    "gap-10",
+                ])}
+            >
+                <h1
+                    className={cn([
+                        "text-2xl",
+                        "font-bold",
+                        "flex",
+                        "gap-2",
+                        "items-center",
+                    ])}
+                >
+                    <Flag />
+                    比赛管理
+                </h1>
+                <div
+                    className={cn([
+                        "flex",
+                        "flex-1",
+                        "justify-center",
+                        "items-center",
+                        "gap-3",
+                    ])}
+                >
+                    <Input
+                        size={"sm"}
+                        placeholder="ID"
+                        icon={HashIcon}
+                        value={
+                            (table
+                                .getColumn("id")
+                                ?.getFilterValue() as number) ?? ""
+                        }
+                        onChange={(e) =>
+                            table
+                                .getColumn("id")
+                                ?.setFilterValue(e.target.value)
+                        }
+                        className={cn(["flex-1"])}
+                    />
+                    <Input
+                        size={"sm"}
+                        placeholder={"比赛名"}
+                        icon={TypeIcon}
+                        value={
+                            (table
+                                .getColumn("title")
+                                ?.getFilterValue() as string) ?? ""
+                        }
+                        onChange={(e) =>
+                            table
+                                .getColumn("title")
+                                ?.setFilterValue(e.target.value)
+                        }
+                        className={cn(["flex-1"])}
+                    />
+                    <Button
+                        icon={PlusCircle}
+                        variant={"tonal"}
+                        onClick={() => setCreateDialogOpen(true)}
+                    >
+                        添加比赛
+                    </Button>
+                    <Dialog
+                        open={createDialogOpen}
+                        onOpenChange={setCreateDialogOpen}
+                    >
+                        <DialogContent>
+                            <CreateDialog
+                                onClose={() => setCreateDialogOpen(false)}
+                            />
+                        </DialogContent>
+                    </Dialog>
+                </div>
+            </div>
+            <div className={cn(["rounded-md", "border", "bg-card"])}>
+                <Table className={cn(["text-foreground"])}>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {!header.isPlaceholder &&
+                                                flexRender(
+                                                    header.column.columnDef
+                                                        .header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    );
+                                })}
+                            </TableRow>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows?.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.getValue("id")}
+                                    data-state={
+                                        row.getIsSelected() && "selected"
+                                    }
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell
+                                    colSpan={columns.length}
+                                    className="h-24 text-center"
+                                >
+                                    哎呀，好像还没有比赛呢。
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+                <div className="flex items-center justify-between space-x-2 py-4 px-4">
+                    <div className="flex-1 text-sm text-muted-foreground">
+                        {table.getFilteredRowModel().rows.length} / {total}
+                    </div>
+                    <div className={cn(["flex", "items-center", "gap-5"])}>
+                        <Select
+                            size={"sm"}
+                            placeholder={"每页显示"}
+                            icon={ListOrderedIcon}
+                            className={cn(["w-48"])}
+                            options={[
+                                { value: "20" },
+                                { value: "40" },
+                                { value: "60" },
+                            ]}
+                            value={String(size)}
+                            onValueChange={(value) => setSize(Number(value))}
+                        />
+                        <Pagination
+                            size={"sm"}
+                            value={page}
+                            total={Math.ceil(Math.ceil(total / size))}
+                            onChange={setPage}
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
