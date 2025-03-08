ce194960310b9db14b4f8572ee163593bf4da8d5
埃拉 <ElaBosak233@qq.com>
2025-03-08 16:53:20 +0800

    wip: update

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import "@fontsource/ubuntu-mono/index.css";
-import "@fontsource/ubuntu/index.css";
 import "@/styles/main.css";
 
 createRoot(document.getElementById("root")!).render(
